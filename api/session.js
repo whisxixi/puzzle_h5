6c1371a8ed85162eb3fecb9adc2425474c94f5b0
whisxixi <cryptoxixi@gmail.com>
2025-03-25 12:52:53 +0800

    test

--- api/session.js
+++ api/session.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
-module.exports = (req, res) => {
-  res.status(200).json({ msg: "Serverless function works with express!" });
-};
+app.get('/', (req, res) => {
+  return res.json({ foo: 'bar' });
+});
+
+module.exports = (req, res) => app(req, res);
