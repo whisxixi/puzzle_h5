507432e8b4e1f64a630af92fbefec3679a058ef9
whisxixi <cryptoxixi@gmail.com>
2025-03-25 13:11:11 +0800

    test

--- api/session.js
+++ api/session.js
@@ -1,11 +1,17 @@
+console.log('===> session.js: before require express');
+
 const express = require('express');
+console.log('===> session.js: after require express');
+
 const app = express();
+console.log('===> session.js: after app created');
 
 app.get('/', (req, res) => {
+  console.log('===> session.js: handling GET /');
   return res.json({ foo: 'bar' });
 });
 
-// module.exports = (req, res) => app(req, res);
 module.exports = (req, res) => {
-  res.status(200).json({ msg: "Serverless function works!" });
+  console.log('===> session.js: exports function called');
+  return app(req, res);
 };
